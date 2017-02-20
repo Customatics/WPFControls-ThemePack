0fee7431799420583ee90c814a64a5968c1f6842
Eugene Kurban <eugene_kurban@tut.by>
2017-02-20 18:36:58 +0300

    Added selected tab settings

--- Themes/ThemeWindow/Windows/MainWindow.xaml.cs
+++ Themes/ThemeWindow/Windows/MainWindow.xaml.cs
@@ -12,12 +12,17 @@ namespace ThemeWindow.Windows
         public MainWindow()
         {
             InitializeComponent();
+            LoadSettings();
+        }
+
+        private void LoadSettings()
+        {
             this.Height = Settings.Default.WindowHeight;
             this.Width = Settings.Default.WindowWidth;
             this.Top = Settings.Default.WindowTop;
             this.Left = Settings.Default.WindowLeft;
             this.WindowState = Settings.Default.WindowState;
-
+            this.tabControl.SelectedIndex = Settings.Default.SelectedTabIndex;
         }
 
         private void Window_Closing(object sender,
@@ -26,9 +31,9 @@ namespace ThemeWindow.Windows
             Settings.Default.WindowHeight = this.Height;
             Settings.Default.WindowWidth = this.Width;
             Settings.Default.WindowTop = this.Top;
-            Settings.Default.WindowLeft = this.Left;
+            Settings.Default.WindowLeft = this.Left < 0 ? 0 : this.Left;
             Settings.Default.WindowState = this.WindowState;
-
+            Settings.Default.SelectedTabIndex = tabControl.SelectedIndex;
             Settings.Default.Save();
         }
     }
